05303570b42d357e62f63f003f3e488220f67c35
nachomanriqueperez <joseim@zeta40.aulas.gsyc.es>
2016-03-08 14:30:50 +0100

    Haciendo web worker

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
 //    self.postMessage( r );  // Contestamos con la suma
 //}, false);
 
-self.addEventListener('message',function buscarPrimos() {
+self.addEventListener('message',function (e) {
   self.postMessage("HAsta aqui");
   var n = 1;
   primelist = "";
