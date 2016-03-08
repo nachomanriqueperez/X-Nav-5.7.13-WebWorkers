81306d8711652166f8b58ec9ab0630f3b57b3ca4
nachomanriqueperez <joseim@zeta40.aulas.gsyc.es>
2016-03-08 14:29:23 +0100

    Haciendo web worker

--- worker.js
+++ worker.js
@@ -10,7 +10,7 @@ self.addEventListener('message',function buscarPrimos() {
   self.postMessage("HAsta aqui");
   var n = 1;
   primelist = "";
-  search: while (n<e.data {
+  search: while (n<e.data) {
     n += 1;
     for (var i = 2; i <= Math.sqrt(n); i += 1)
       if (n % i == 0)
